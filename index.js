e37f4b49162489570a07eb8657408b8770d8005b
benholloway <ben.holloway@3plearning.com>
2016-06-01 13:48:21 +1000

    drafting out a major rewrite

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 'use strict';
 
-var path = require('path'),
-    fs   = require('fs');
-
 var PACKAGE_NAME = require('./package.json').name;
 
 function OmitTildeWebpackPlugin(options) {
@@ -11,28 +8,15 @@ function OmitTildeWebpackPlugin(options) {
 module.exports = OmitTildeWebpackPlugin;
 
 OmitTildeWebpackPlugin.prototype.apply = function apply(compiler) {
-  var options  = this.options,
-      warnings = [];
-
-  // work out the modules that we need to respond to
-  var excludeNames = [].concat(options.exclude)
-    .map(String);
-
-  var includeNames = [].concat(options.include)
-    .map(String)
-    .reduce(eachInclude, []);
-
-  var moduleNames = includeNames
-    .filter(testNotExcluded.bind(null, excludeNames));
+  var options      = this.options,
+      warnings     = [],
+      recursionMap = {};
 
   // hook the compiler so we can push warnings in the warn() method
   compiler.plugin('after-compile', afterCompile);
 
-  if (moduleNames.length) {
-    compiler.resolvers.normal.plugin('directory', directoryResolver);
-  } else {
-    warn('No dependencies found, plugin will not run');
-  }
+  // add additional directory resolver
+  compiler.resolvers.normal.plugin('directory', directoryResolver);
 
   function warn() {
     var text = [PACKAGE_NAME]
@@ -44,84 +28,64 @@ OmitTildeWebpackPlugin.prototype.apply = function apply(compiler) {
   }
 
   function afterCompile(compilation, done) {
+    recursionMap = {};
     compilation.warnings.push.apply(compilation.warnings, warnings);
     done();
   }
 
-  function eachInclude(reduced, include) {
-    if (/\.json$/i.test(include)) {
-      var contents;
-      try {
-        contents = require(path.resolve(include));
-      }
-      catch (exception) {
-        warn('file', include, 'was not found in the working directory');
-      }
-      return reduced
-        .concat(contents && contents.dependencies && Object.keys(contents.dependencies))
-        .concat(contents && contents.devDependencies && Object.keys(contents.devDependencies))
-        .concat(contents && contents.bundledDependencies)
-        .filter(Boolean)
-        .reduce(flatten, []);
-    }
-    else {
-      return reduced.concat(include);
-    }
-
-    function flatten(reduced, value) {
-      return reduced.concat(value);
-    }
-  }
-
-  function testNotExcluded(excluded, candidate) {
-    return (excluded.indexOf(candidate) < 0);
-  }
-
   function directoryResolver(candidate, done) {
     /* jshint validthis:true */
+    var doResolve = this.doResolve;
 
-    // check for match
+    // ignore recursions
     var requestText = candidate.request,
-        regex       = options.test,
-        isTested    = !regex ||
-          ((typeof regex === 'object') && (typeof regex.test === 'function') && regex.test(requestText));
-
-    // determine whether it is a module
-    var split      = isTested && requestText.split(/[\\\/]+/),
-        isRelative = !!split && (split[0] === '.');
-
-    // determine whether the request is prefixed with what looks to be a module
-    var isMatched = isRelative && (moduleNames.indexOf(split[1]) >= 0);
-
-    //  ensure that the request is not an immediate file (i.e. filename looks like package name)
-    var naivePath = path.normalize(path.join(candidate.path, candidate.request)),
-        isOperate = isMatched && !fs.existsSync(naivePath);
-
-    // operate
-    //  amend and re-issue the request as a module
-    if (isOperate) {
-      var amended = {
-        path   : candidate.path,
-        request: requestText.slice(2),
-        query  : candidate.query,
-        module : true
-      };
-      this.doResolve(['module'], amended, options.deprecate ? resolved : done);
-    }
-    // ignore
-    else {
+        isRecursing = (requestText in recursionMap);
+    if (isRecursing) {
       done();
     }
-
-    function resolved(error, result) {
-      if (!error && result) {
-        warn([
-          'files should use ~ to refer to modules:',
-          '  in directory: "' + candidate.path + '"',
-          '  change "' + amended.request + '" -> "~' + amended.request + '"'
-        ].join('\n'));
-      }
-      done(error, result);
+    // repeat the request, but this time we control the callbacks
+    else {
+      recursionStart(requestText);
+      doResolve(['directory'], candidate, onDirectoryResolve);
     }
+
+    function onDirectoryResolve(error, result) {
+      recursionEnd(requestText);
+      var amended;
+
+      // the original request would have been successful
+      if (!error && result) {
+        done(error, result);
+      }
+      // relaunch the request as a module, removing any relative path prefix
+      else {
+        amended = {
+          path   : candidate.path,
+          request: requestText.replace(/^\.+[\\\/]/, ''),
+          query  : candidate.query,
+          module : true
+        };
+        doResolve(['module'], amended, options.deprecate ? resolved : done);
+      }
+
+      function resolved(error, result) {
+        if (!error && result) {
+          warn([
+            'files should use ~ to refer to modules:',
+            '  in directory: "' + candidate.path + '"',
+            '  change "' + amended.request + '" -> "~' + amended.request + '"'
+          ].join('\n'));
+        }
+        done(error, result);
+      }
+    }
+  }
+
+  function recursionStart(key) {
+    recursionMap[key] = true;
+  }
+
+  function recursionEnd(key) {
+    recursionMap[key] = false;
   }
 };
